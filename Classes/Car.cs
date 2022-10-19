1a4af2ac7dc6e1bd0034db5e7cdb49f0c5de6351
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-10-19 13:07:11 -0400

    completed

---
+++ Classes/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public string Make { get; set; }
+
+        public int Year { get; set; }
+
+        public string Model { get; set; }
+
+        public void CustomerGreeting()
+        {
+            Console.WriteLine($"Welcome {Name}. We have you checked in.");
+        }
+        public void CusotmerCheckOut()
+        {
+            Console.WriteLine($"{Name} we have a your car as a {Year} {Make} {Model} ready to go.");
+        }
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Can we get some info about you first?");
+            Console.WriteLine("Whats your Name?");
+            var Customer = new Car();
+            Customer.Name = Console.ReadLine();
+            Console.WriteLine("And what Model is it");
+            Customer.Make = Console.ReadLine();
+            Console.WriteLine("And the Year?");
+            Customer.Year = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What Model");
+            Customer.Model = Console.ReadLine();
+
+            Customer.CustomerGreeting();
+            Customer.CusotmerCheckOut();
         }
     }
 }
